50fb052e1538e329e6c0b06f52d5eb24b6e7dbb5
yourfriend1111 <166567307+yourfriend1111@users.noreply.github.com>
2025-09-18 16:41:40 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,20 +1,24 @@
 import chess
 
-# 1. Setup the board
+
 base_board = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 board = chess.Board(base_board)
 
-# Optional: Print the board before the move
-print("--- Board before move ---")
-print(board)
 
-# 2. Create the move object (e2 to e4)
-move = chess.Move(from_square=chess.E2, to_square=chess.E4)
-print(f"\nMove object created: {move}")
 
-# 3. Apply the move to the board
-board.push(move)
 
-# Optional: Print the board after the move
-print("\n--- Board after move ---")
-print(board)
+def set_move(desired_move):
+    global move
+    move = desired_move
+
+
+def chess_move():
+    board.push(move)
+    print(board)
+
+set_move(chess.Move(from_square=chess.E2, to_square=chess.E4))
+chess_move()
+
+set_move(chess.Move(from_square=chess.E7, to_square=chess.E5))
+chess_move()
+
