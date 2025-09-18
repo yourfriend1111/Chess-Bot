0ca8b2b424443cc2ae394b2b03ca5db8898a892b
yourfriend1111 <166567307+yourfriend1111@users.noreply.github.com>
2025-09-18 17:15:07 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,24 +1,38 @@
 import chess
 
-
 base_board = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
-board = chess.Board(base_board)
-
-
-
-
-def set_move(desired_move):
-    global move
-    move = desired_move
-
 
 def chess_move():
+    global board
     board.push(move)
     print(board)
+    
+def start():
+    global board
+    board = chess.Board(base_board)
+    print(board)
+    move_choice()
+    
+def move_choice():
+    global move
+    starting_square = input('What sqaure are you moving from? ')
+    if (starting_square == 'reset'):
+        start()
+    ending_square = input('What sqaure are you moving to? ')
+    
+    if (ending_square == 'reset'):
+        start()
+    else:
+        desired_move = chess.Move.from_uci(starting_square + ending_square)
+        if (board.is_legal(desired_move) == True):
+            move = desired_move
+            chess_move()
+        else:
+            print('This is an illegal move!')
+        move_choice()
+        
+        
+    
+start()
 
-set_move(chess.Move(from_square=chess.E2, to_square=chess.E4))
-chess_move()
-
-set_move(chess.Move(from_square=chess.E7, to_square=chess.E5))
-chess_move()
 
